9f885851d3d3ac2cd557da7ebc546d4b82f6ed84
Evan Wu <ewu43951@gmail.com>
2026-06-09 12:03:17 -0500

    add logger to main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 # Date of Revision: 6/9/2026
 
 
+
 from scapy.all import *
+import logging
+
+# Set log level to INFO to take advantage of Scapy warnings
+logger = logging.getLogger("scapy")
+logger.setLevel(logging.INFO)
 
 
 def main() -> None:
