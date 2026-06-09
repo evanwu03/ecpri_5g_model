3794b4a0bdd3cb3864a67d7d9043545f1a1f532b
Evan Wu <ewu43951@gmail.com>
2026-06-09 12:00:47 -0500

    defined ECPRI packet class

---
+++ ecpri.py
@@ -0,0 +1,12 @@
+# Author: Evan Wu
+# Date of Revision: 6/9/2026
+
+from scapy.all import Packet
+
+
+
+class ECPRI(Packet):
+    name = "ECPRI"
+    
+
+    
---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Author: Evan Wu
+# Date of Revision: 6/9/2026
+
+
+from scapy.all import *
+
+
+def main() -> None:
+    interact(mydict=globals())
+
+
+if __name__=="__main__":
+    main()
